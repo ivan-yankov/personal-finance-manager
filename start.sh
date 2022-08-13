a95280134cb52480d604596273cf515d3cbb5381
Ivan <ivan.iankov@gmail.com>
2022-08-13 20:07:48 +0300

    Fix

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -Xms2G -Xmx6G -Dfile.encoding=UTF-8 -classpath "jar/*" org.yankov.console.editor.Main income.csv expense.csv
+java -Xms2G -Xmx6G -Dfile.encoding=UTF-8 -classpath "jar/*" org.yankov.finance.manager.Main income.csv expense.csv
